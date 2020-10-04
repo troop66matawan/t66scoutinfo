8b8a8d5db527bb9d0843ab61574c4b01416357d4
troop66matawan <16478845+troop66matawan@users.noreply.github.com>
2020-10-04 17:14:18 -0400

    add additional imports

--- import/index.js
+++ import/index.js
@@ -1,6 +1,8 @@
 const ScoutImporter = require('scoutbook-scouts-importer');
 const AdvancementImporter = require('scoutbook-advancement-importer');
 const ActivityImporter = require('scoutbook-activities-importer');
+const LeadershipImporter = require('scoutbook-leadership-importer');
+const IAActivityImporter = require('ia-activity-importer');
 const admin = require('firebase-admin');
 
 const t66App = admin.initializeApp({
@@ -10,8 +12,8 @@ const t66App = admin.initializeApp({
 const dbInst = t66App.database();
 const t66scoutbookDb = dbInst.ref('scoutbookDb');
 
-if (process.argv.length !== 5) {
-    console.log('Usage: ' + process.argv[1] + ' <scoutbook__scouts.csv file to import> <scoutbook_advancement.csv file to import> <scoutbook_log.csv file to import>');
+if (process.argv.length !== 7) {
+    console.log('Usage: ' + process.argv[1] + ' <scoutbook__scouts.csv file to import> <scoutbook_advancement.csv file to import> <scoutbook_log.csv file to import> <scoutbook_leadership.csv file to import> <ia-activity-export.csv to import>');
 } else {
 
     ScoutImporter.scoutbook_scouts_importer(process.argv[2])
@@ -19,9 +21,16 @@ if (process.argv.length !== 5) {
             AdvancementImporter.scoutbook_advancement_importer(scouts, process.argv[3])
                 .then(function(scoutsWithAdv) {
                     ActivityImporter.scoutbook_activities_importer(scouts, process.argv[4])
-                        .then(function(finalScouts) {
-                            t66scoutbookDb.update(finalScouts);
-                        });
+                        .then(function(scoutsWithAct) {
+                            IAActivityImporter.scoutbook_ia_activities_importer(scoutsWithAct, process.argv[6])
+                                .then(function(scoutsWithIA) {
+                                    LeadershipImporter.scoutbook_leadership_importer(scoutsWithIA, process.argv[5])
+                                        .then(function(finalScouts) {
+                                            t66scoutbookDb.update(finalScouts);
+                                        });
+                                })
+                        })
+
                 })
         })
         .catch(function (err) {
